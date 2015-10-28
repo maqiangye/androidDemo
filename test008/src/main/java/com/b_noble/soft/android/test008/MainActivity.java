ff63fa2802d779f214524bb62fc0215b8133614d
maqiangye <maqiangye@163.com>
2015-10-28 20:34:28 +0800

    first commit test008

--- test008/src/main/java/com/b_noble/soft/android/test008/MainActivity.java
+++ test008/src/main/java/com/b_noble/soft/android/test008/MainActivity.java
@@ -1,11 +1,10 @@
-package com.b_noble.soft.android.test005;
+package com.b_noble.soft.android.test008;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        Log.d("test005","test005");
     }
 
     @Override
