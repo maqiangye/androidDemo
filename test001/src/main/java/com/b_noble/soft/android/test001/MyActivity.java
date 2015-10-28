ac74395f8c9a23fc13ae4b8f6237e73324f0a82e
maqiangye <maqiangye@163.com>
2015-10-28 14:13:36 +0800

    first commit

---
+++ test001/src/main/java/com/b_noble/soft/android/test001/MyActivity.java
@@ -0,0 +1,16 @@
+package com.b_noble.soft.android.test001;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mqy on 15/10/28.
+ */
+public class MyActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mylayout);
+    }
+}
